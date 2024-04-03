89cb4ce053e3ea741ea31f6c62bf9a86a6fc91f5
María Frausto <mfrausto.fs@gmail.com>
2024-04-03 14:34:47 -0700

    changing to basics

--- leap-year.js
+++ leap-year.js
@@ -1,12 +1,18 @@
-let year = 2024;
+document.getElementById("checkButton").addEventListener("click", function () {
+    var year = parseInt(document.getElementById("year").value);
+    var resultElement = document.getElementById("result");
 
-if(year%4==0){
-    if(year%100==0){
-        console.log("The year "+year +" is NOT a leap year.");
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                resultElement.textContent = year + " is a leap year.";
+            } else {
+                resultElement.textContent = year + " is NOT a leap year.";
+            }
+        } else {
+            resultElement.textContent = year + " is a leap year.";
+        }
     } else {
-        console.log("The year "+ year + " is a leap year.")
+        resultElement.textContent = year + " is NOT a leap year.";
     }
-} else{
-
-console.log("The year "+year + " is NOT a leap year");
-}
+});

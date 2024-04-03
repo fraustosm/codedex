22ec825e15cf1fd003d4bbfeea49f98bccc33445
María Frausto <mfrausto.fs@gmail.com>
2024-04-03 13:27:21 -0700

    exercise finished w/o styles

---
+++ leap-year.js
@@ -0,0 +1,12 @@
+let year = 2024;
+
+if(year%4==0){
+    if(year%100==0){
+        console.log("The year "+year +" is NOT a leap year.");
+    } else {
+        console.log("The year "+ year + " is a leap year.")
+    }
+} else{
+
+console.log("The year "+year + " is NOT a leap year");
+}
